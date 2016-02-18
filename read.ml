5aa514a0abf561d2f6b7f8d4706839c551a38229
ivanplex <ivanplex@gmail.com>
2016-02-18 13:00:03 +0000

    Read file

--- read.ml
+++ read.ml
@@ -1,17 +1,8 @@
-open Printf
-
-let file = "file.spl"
-  
-let () =
-(* Read file and display the first line *)
-  let ic = open_in file in
-    try 
-      let line = input_line ic in  (* read line from in_channel and discard \n *)
-      print_endline line;          (* write the result to stdout *)
-      flush stdout;                (* write on the underlying device now *)
-      close_in ic                  (* close the input channel *) 
-
-    with e ->                      (* some unexpected exception occurs *)
-      close_in_noerr ic;           (* emergency closing *)
-      raise e                      (* exit with error: files are closed but
-                                      channels are not flushed *)
+let in_channel = open_in "file.spl" in
+try
+  while true do
+    let line = input_line in_channel in
+    print_endline line;
+  done
+with End_of_file ->
+  close_in in_channel
