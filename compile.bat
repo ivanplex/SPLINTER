1f2211d2af408f2be25ad375fc64c7d95dc90b17
Stewart Paterson <sp16g14@soton.ac.uk>
2016-04-27 23:44:54 +0100

    Commented/refactored 6, 8, 9, 10

--- compile.bat
+++ compile.bat
@@ -1,53 +1,10 @@
 @echo off
-echo compiling SPL
-
-
-:: produces spllexer.ml
-echo spllexer.mll
-ocamllex spllexer.mll
-
-:: produces splparser.ml & splparser.mli
-echo splparser.mly
-ocamlyacc splparser.mly
-
-:: produces spl.cmi
-echo spl.mli
-ocamlc -c spl.mli
-
-:: produces splparser.cmi (?)
-echo splparser.mli
-ocamlc -c splparser.mli
-
-:: produces splparser.cmo (?)
-echo splparser.ml
-ocamlc -c splparser.ml
-
-:: produces spllexer.cmo & spllexer.cmi
-echo spllexer.ml
-ocamlc -c spllexer.ml
-
-:: produces spl.cmo
-echo spl.ml
-ocamlc -c spl.ml
-
-:: produces main.cmo & main.cmi
-echo main.ml
-ocamlc -c main.ml
-
-echo linking executable
-
-:: produces splinterpreter.exe
-ocamlc -o splinterpreter.exe  splparser.cmo spllexer.cmo spl.cmo main.cmo
-
-echo removing temporary files
-del spllexer.ml
-del splparser.ml
-del splparser.mli
-del spl.cmi
-del splparser.cmi
-del splparser.cmo
-del spllexer.cmi
-del spllexer.cmo
-del spl.cmo
-del main.cmo
-del main.cmi
+make
+if %errorlevel%==0 ( 
+	if exist mysplinterpreter (
+		if exist mysplinterpreter.exe ( 
+			del mysplinterpreter.exe
+		)
+		rename mysplinterpreter mysplinterpreter.exe
+	)
+)
